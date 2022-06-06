e6e6036441355f0e4118b7d5f4666647d83fa95d
facundo <fmabeldano@dc.uba.ar>
2022-06-05 21:16:23 -0300

    Cambios:
    -Añadidas las templates en settings
    -Modificadas las views para testeo (temporal)

--- MueblesJuan/settings.py
+++ MueblesJuan/settings.py
@@ -55,7 +55,7 @@ ROOT_URLCONF = 'MueblesJuan.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': ['/home/facundo/Documents/Python Projects/tp_grupal_coder/TP-Grupal-CH/products/templates/'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
--- MueblesJuan/views.py
+++ MueblesJuan/views.py
@@ -2,7 +2,7 @@ from django.shortcuts import render
 
 
 def home(request):
-    return
+    return render(request=request, template_name='index.html')
 
 
 def faq(request):
