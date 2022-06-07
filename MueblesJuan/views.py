20451592940be8d4f2f16cb5807f4a627d4c7948
facundo <fmabeldano@dc.uba.ar>
2022-06-06 22:17:09 -0300

    Re-wiring templates

--- MueblesJuan/views.py
+++ MueblesJuan/views.py
@@ -2,10 +2,10 @@ from django.shortcuts import render
 
 
 def home(request):
-    template = 'products/index_template_0.html'
+    template = 'products.html'
     return render(request=request, template_name=template)
 
 
 def faq(request):
-    template = 'products/faq.html'
+    template = 'faq.html'
     return render(request=request, template_name=template)
