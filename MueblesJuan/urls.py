8f7b389b056077ffb8d8972b4b5374f4c51b7fdd
facundo <fmabeldano@dc.uba.ar>
2022-06-06 22:47:54 -0300

    Agregada view de order, modificaciones en rewiring

--- MueblesJuan/urls.py
+++ MueblesJuan/urls.py
@@ -16,11 +16,12 @@ Including another URLconf
 
 from django.contrib import admin
 from django.urls import include, path
-from .views import home, faq
+from .views import *
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', home, name='home'),
     path('faq/', faq, name='faq'),
+    path('order/', order, name='order'),
     path('productos/', include('products.urls')),
 ]
--- MueblesJuan/views.py
+++ MueblesJuan/views.py
@@ -3,9 +3,17 @@ from django.shortcuts import render
 
 def home(request):
     template = 'index.html'
-    return render(request=request, template_name=template)
+    context_payload = {}
+    return render(request=request, template_name=template, context=context_payload)
 
 
 def faq(request):
     template = 'faq.html'
-    return render(request=request, template_name=template)
+    context_payload = {}
+    return render(request=request, template_name=template, context=context_payload)
+
+
+def order(request):
+    template = 'order.html'
+    context_payload = {}
+    return render(request=request, template_name=template, context=context_payload)
