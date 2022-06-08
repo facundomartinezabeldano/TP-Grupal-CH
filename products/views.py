80b3cb6857bd2804bb4c0075bf8f5263a208889d
facundo <fmabeldano@dc.uba.ar>
2022-06-08 19:38:40 -0300

    Añadida views de productos

--- products/views.py
+++ products/views.py
@@ -1,10 +1,18 @@
 from django.http import HttpResponse
 from django.shortcuts import render
+from .models import *
+from itertools import chain
 
 # Create your views here.
 
 
 def products(request):
     view = 'products.html'
-    context = {}
-    return render(request=request, template_name=view, context=context)
+    sillas = Silla.objects.all()
+    mesas = Mesa.objects.all()
+    escritorios = Escritorio.objects.all()
+    sillones = Sillon.objects.all()
+    res = list(chain(sillas, mesas, escritorios, sillones))
+    payload = {'products': res}
+    print(res)
+    return render(request=request, template_name=view, context=payload)
