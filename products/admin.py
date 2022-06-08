4102074b0a3bb1984f8908e6eeaee737d5364e45
facundo <fmabeldano@dc.uba.ar>
2022-06-08 19:39:01 -0300

    Registrados modelos en el admin

--- products/admin.py
+++ products/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
+from .models import *
 
-# Register your models here.
+admin.site.register(Silla)
+admin.site.register(Escritorio)
+admin.site.register(Sillon)
+admin.site.register(Mesa)
