d94d1b12d1d06f791054dbdcdf48b8c205c44117
facundo <fmabeldano@dc.uba.ar>
2022-06-08 20:46:19 -0300

    Agregada view de formulario

--- MueblesJuan/views.py
+++ MueblesJuan/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
 from .forms import OrderForm
+from products.models import Orden
 
 
 def home(request):
@@ -16,6 +17,18 @@ def faq(request):
 
 def order(request):
     template = 'order.html'
-    if request.method == 'POST':
-        orden = OrderForm()
-    return render(request=request, template_name=template, context={})
+
+    my_form = OrderForm(request.POST)
+    if my_form.is_valid:
+        payload = my_form.cleaned_data
+        order = Orden(
+            name=payload['name'],
+            email=payload['email'],
+            producto=payload['product'],
+            message=payload['message'],
+        )
+        order.save()
+    else:
+        my_form = OrderForm()
+
+    return render(request=request, template_name=template, context=my_form)
